ab56a737714e3a36ce0b3ed3c39e72ea1cdac29e
Krishna Softweb <developer.krishnasoftweb@gmail.com>
2023-01-16 17:41:17 +0530

    Created new schema

---
+++ controllers/branch/leave.js
@@ -0,0 +1,67 @@
+const leaveService = require("../../services/leave");
+
+async function find(req, res) {
+  res.json({
+    status: true,
+    message: "",
+    data: await leaveService.find(),
+  });
+}
+
+async function findById(req, res) {
+  res.json({
+    status: true,
+    message: "",
+    data: await leaveService.findById(req.params.id),
+  });
+}
+
+async function create(req, res) {
+  try {
+    res.json({
+      status: true,
+      message: "",
+      data: await leaveService.create(req.body),
+    });
+  } catch (err) {
+    res.json({
+      status: false,
+      message: err.message,
+      data: {},
+    });
+  }
+}
+
+async function update(req, res) {
+  try {
+    res.json({
+      status: true,
+      message: "",
+      data: await leaveService.update(req.params.id, req.body),
+    });
+  } catch (err) {
+    res.json({
+      status: false,
+      message: err.message,
+      data: {},
+    });
+  }
+}
+
+async function remove(req, res) {
+  try {
+    res.json({
+      status: true,
+      message: "",
+      data: await leaveService.remove(req.params.id),
+    });
+  } catch (err) {
+    res.json({
+      status: false,
+      message: err.message,
+      data: {},
+    });
+  }
+}
+
+module.exports = { find, findById, create, update, remove };
--- routes/branch.js
+++ routes/branch.js
@@ -9,6 +9,9 @@ const expense = require("../controllers/branch/expense");
 const employee = require("../controllers/branch/employee");
 const profile = require("../controllers/branch/profile");
 const sales = require("../controllers/branch/sales");
+const release = require("../controllers/branch/release");
+const attendance = require("../controllers/branch/attendance");
+const leave = require("../controllers/branch/leave");
 const { isBranch } = require("../middlewares/authorization");
 
 authRouter.post("/auth/login", login);
@@ -35,11 +38,29 @@ fundRouter.post("/sales/create", sales.create);
 fundRouter.post("/sales/update/:id", sales.update);
 fundRouter.post("/sales/delete/:id", sales.remove);
 
+fundRouter.get("/release/get", release.find);
+fundRouter.get("/release/get/:id", release.findById);
+fundRouter.post("/release/create", release.create);
+fundRouter.post("/release/update/:id", release.update);
+fundRouter.post("/release/delete/:id", release.remove);
+
 fundRouter.get("/employee/get", employee.find);
 fundRouter.get("/employee/get/:id", employee.findById);
 fundRouter.post("/employee/create", employee.create);
 fundRouter.post("/employee/update/:id", employee.update);
 fundRouter.post("/employee/delete/:id", employee.remove);
+
+fundRouter.get("/attendance/get", attendance.find);
+fundRouter.get("/attendance/get/:id", attendance.findById);
+fundRouter.post("/attendance/create", attendance.create);
+fundRouter.post("/attendance/update/:id", attendance.update);
+fundRouter.post("/attendance/delete/:id", attendance.remove);
+
+fundRouter.get("/leave/get", leave.find);
+fundRouter.get("/leave/get/:id", leave.findById);
+fundRouter.post("/leave/create", leave.create);
+fundRouter.post("/leave/update/:id", leave.update);
+fundRouter.post("/leave/delete/:id", leave.remove);
 
 fundRouter.get("/profile", profile.get);
 fundRouter.post("/profile/change-password", profile.changePassword);
