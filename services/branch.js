c22bb4211377cf0d28ea125fffe913ba1344a8ac
Krishna Softweb <developer.krishnasoftweb@gmail.com>
2023-01-23 11:19:58 +0530

    dfA

--- services/branch.js
+++ services/branch.js
@@ -37,7 +37,11 @@ async function update(id, payload) {
 
 async function remove(id) {
   try {
-    return await Branch.findByIdAndDelete(id).exec();
+    return await Branch.deleteMany({
+      _id: {
+        $in: id.split(","),
+      },
+    }).exec();
   } catch (err) {
     throw err;
   }
