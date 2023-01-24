bc7c527096ad8a2b7779c3d857985516d7991c32
Krishna Softweb <developer.krishnasoftweb@gmail.com>
2023-01-24 12:36:13 +0530

    File upload

--- config/multer.js
+++ config/multer.js
@@ -1,4 +1,2 @@
 var multer = require("multer");
-var path = require("path");
-const upload = multer({ dest: path.join(__dirname, "public") });
-module.exports = upload;
+module.exports = multer({ dest: "../storage" });
