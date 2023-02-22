9828ddf9b70881c43dfcdb3f93d66accfdb6b88e
Developer <developer.krishnasoftweb@gmail.com>
2023-02-22 13:29:44 +0530

    dfdFD

--- routes/customer.js
+++ routes/customer.js
@@ -8,11 +8,16 @@ const {
 } = require("../controllers/customer/auth");
 const profile = require("../controllers/customer/profile");
 const goldRate = require("../controllers/customer/goldrate");
+const branch = require("../controllers/customer/branch");
 
 router.post("/login", login);
 router.post("/verify-otp", verifyOtp);
 
 customerRouter.post("/goldrate", goldRate.findOne);
+
+customerRouter.get("/branch/get", branch.find);
+customerRouter.get("/branch/get/:id", branch.findById);
+customerRouter.post("/branch/find", branch.findOne);
 
 customerRouter.get("/profile", profile.get);
 customerRouter.post("/profile", profile.update);
