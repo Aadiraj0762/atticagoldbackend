152882fcc53a35df904370822917c5b10c9c69bd
Developer <developer.krishnasoftweb@gmail.com>
2023-04-27 10:50:18 +0530

    dfgdsA

--- controllers/branch/sales.js
+++ controllers/branch/sales.js
@@ -10,11 +10,19 @@ async function find(req, res) {
 }
 
 async function findById(req, res) {
-  res.json({
-    status: true,
-    message: "",
-    data: await salesService.findById(req.params.id),
-  });
+  try {
+    res.json({
+      status: true,
+      message: "",
+      data: await salesService.findById(req.params.id),
+    });
+  } catch (err) {
+    res.json({
+      status: false,
+      message: err.errors ?? err.message,
+      data: {},
+    });
+  }
 }
 
 async function create(req, res) {
