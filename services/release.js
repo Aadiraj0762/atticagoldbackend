a8f147ddd1f6b76c0d5770e668c14c53c87feea7
Developer <developer.krishnasoftweb@gmail.com>
2023-05-29 12:54:44 +0530

    Filter api

--- services/release.js
+++ services/release.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 
 async function find(query = {}) {
   try {
+    let filter = {};
     if (query.createdAt && "$gte" in query.createdAt) {
       query.createdAt["$gte"] = new Date(query.createdAt["$gte"]);
     }
@@ -14,12 +15,70 @@ async function find(query = {}) {
     } else {
       delete query.branch;
     }
+    if (query.phoneNumber) {
+      filter["customer.phoneNumber"] = query.phoneNumber;
+      delete query.phoneNumber;
+    } else {
+      delete query.phoneNumber;
+    }
     if (query.customer) {
       query.customer = new mongoose.Types.ObjectId(query.customer);
     } else {
       delete query.customer;
     }
-    return await Release.find(query).exec();
+    return await Release.aggregate([
+      {
+        $match: query,
+      },
+      {
+        $lookup: {
+          from: "branches",
+          localField: "branch",
+          foreignField: "_id",
+          as: "branch",
+        },
+      },
+      {
+        $lookup: {
+          from: "customers",
+          localField: "customer",
+          foreignField: "_id",
+          pipeline: [
+            {
+              $lookup: {
+                from: "fileuploads",
+                localField: "_id",
+                foreignField: "uploadId",
+                as: "profileImage",
+              },
+            },
+            {
+              $addFields: {
+                profileImage: { $first: "$profileImage" },
+              },
+            },
+          ],
+          as: "customer",
+        },
+      },
+      {
+        $lookup: {
+          from: "fileuploads",
+          localField: "_id",
+          foreignField: "uploadId",
+          as: "proof",
+        },
+      },
+      {
+        $addFields: {
+          branch: { $first: "$branch" },
+          customer: { $first: "$customer" },
+        },
+      },
+      {
+        $match: filter,
+      },
+    ]).exec();
   } catch (err) {
     throw err;
   }
