7c0cde66f5d5c0450f16a4bb64f7a68a5d98dc11
Developer <developer.krishnasoftweb@gmail.com>
2023-06-02 12:07:28 +0530

    asdasdfadf

--- controllers/customer/support.js
+++ controllers/customer/support.js
@@ -7,7 +7,7 @@ async function find(req, res) {
   res.json({
     status: true,
     message: "",
-    data: await supportService.find({ customer: req.user._id }),
+    data: await supportService.find(req.body ?? {}),
   });
 }
 
