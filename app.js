a956c6fdbc74e1609b414dd00027e83414744289
Aadiraj0762 <narayaneaadiraj@gmail.com>
2024-04-13 21:33:37 +0530

    backend to upload for hosting

--- app.js
+++ app.js
@@ -38,19 +38,18 @@ app.use("/api/v1.0/customer", customerRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  const err = createError(404);
-  next(err);
+  next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
-  res.render("error");
+  res.json({
+    error: {
+      message: err.message,
+      status: err.status || 500
+    }
+  });
 });
 
 const port = process.env.PORT || 5000;
